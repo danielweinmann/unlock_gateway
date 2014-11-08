325bf547407ee773ad569c04a9a7ad732f5373ea
Daniel Weinmann <danielweinmann@gmail.com>
2014-11-08 12:41:49 -0200

    Documenting the code

--- lib/unlock_gateway/models/contribution.rb
+++ lib/unlock_gateway/models/contribution.rb
@@ -1,10 +1,14 @@
 module UnlockGateway
   module Models
+
+    # This module will be included in Unlock's Contribution model. All methods will run in the context of an instance of Contribution model.
     module Contribution
 
+      # This method should implement a way to check the state of the subscription with the gateway and update the contribution's state according to it's real state within the gateway.
       def update_state_from_gateway!
       end
 
     end
+
   end
 end
